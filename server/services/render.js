00c161105d8a7181d36fb5e53034c964e61adbe2
rb2kGit <59036227+rb2kGit@users.noreply.github.com>
2021-12-18 23:09:25 -0500

    changed to heroku path in services.

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 exports.homeRoutes = (req, res) => {
-    axios.get('http://localhost:3000/api/beasts').then(function(response){
+    axios.get('https://nimreths-beasts.herokuapp.com/api/beasts').then(function(response){
         res.render('index', { beasts : response.data})
     })
     .catch(err => {
@@ -10,7 +10,7 @@ exports.homeRoutes = (req, res) => {
 }
 
 exports.view_beast =  (req, res) => {
-    axios.get('http://localhost:3000/api/beasts', { params : {...req.query}}).then(function(beastdata){
+    axios.get('https://nimreths-beasts.herokuapp.com/api/beasts', { params : {...req.query}}).then(function(beastdata){
         res.render('view-beast', { beast : beastdata.data})
     })
     .catch(err =>{
@@ -19,10 +19,12 @@ exports.view_beast =  (req, res) => {
 }
 
 exports.filter_result =  (req, res) => {
-    axios.get('http://localhost:3000/api/beasts', { params : {...req.query}}).then(function(beastdata){
+    axios.get('https://nimreths-beasts.herokuapp.com/api/beasts', { params : {...req.query}}).then(function(beastdata){
         res.render('view-results', { beasts : beastdata.data})
     })
     .catch(err =>{
         res.send(err)
     })
 }
+
+//http://localhost:3000/api/beasts
